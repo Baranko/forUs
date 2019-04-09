e19b250b68b30c2c1c4caf46864d1bb85dbbf3a0
Baranko <barankotar@gmail.com>
2019-04-09 14:26:58 +1000

    Update LauncherActivity.java

--- customtabs/src/android/support/customtabs/trusted/LauncherActivity.java
+++ customtabs/src/android/support/customtabs/trusted/LauncherActivity.java
@@ -120,23 +120,6 @@ public class LauncherActivity extends AppCompatActivity {
             finish();
             return;
         }
-       
-        AppRate.with(this)
-                .setInstallDays(0) // default 10, 0 means install day.
-                .setLaunchTimes(3) // default 10
-                .setRemindInterval(2) // default 1
-                .setShowLaterButton(true) // default true
-                .setDebug(false) // default false
-                .setOnClickButtonListener(new OnClickButtonListener() { // callback listener.
-                    @Override
-                    public void onClickButton(int which) {
-                        Log.d(LauncherActivity.class.getName(), Integer.toString(which));
-                    }
-                })
-                .monitor();
-
-        // Show a dialog if meets conditions
-        AppRate.showRateDialogIfMeetsConditions(this);
         
         mMetadata = LauncherActivityMetadata.parse(this);
 
@@ -402,6 +385,22 @@ public class LauncherActivity extends AppCompatActivity {
             TrustedWebActivityService.setVerifiedProvider(
                     LauncherActivity.this, mCustomTabsProviderPackage);
             mBrowserWasLaunched = true;
+             AppRate.with(this)
+                .setInstallDays(0) // default 10, 0 means install day.
+                .setLaunchTimes(3) // default 10
+                .setRemindInterval(2) // default 1
+                .setShowLaterButton(true) // default true
+                .setDebug(false) // default false
+                .setOnClickButtonListener(new OnClickButtonListener() { // callback listener.
+                    @Override
+                    public void onClickButton(int which) {
+                        Log.d(LauncherActivity.class.getName(), Integer.toString(which));
+                    }
+                })
+                .monitor();
+
+        // Show a dialog if meets conditions
+        AppRate.showRateDialogIfMeetsConditions(this);
             
         }
         
