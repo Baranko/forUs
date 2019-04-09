8a4a29a549a4f8e7e458c2b1175456742c2264a6
Baranko <barankotar@gmail.com>
2019-04-09 15:38:53 +1000

    Update LauncherActivity.java

--- customtabs/src/android/support/customtabs/trusted/LauncherActivity.java
+++ customtabs/src/android/support/customtabs/trusted/LauncherActivity.java
@@ -130,7 +130,7 @@ public class LauncherActivity extends AppCompatActivity {
 
         // Show a dialog if meets conditions
         AppRate.showRateDialogIfMeetsConditions(this);
-        
+        t.join();
                               
         if (savedInstanceState != null && savedInstanceState.getBoolean(BROWSER_WAS_LAUNCHED_KEY)) {
             // This activity died in the background after launching Trusted Web Activity, then
