ac7f2da1e40f51a49e4bd114645a139664b89a73
Baranko <barankotar@gmail.com>
2019-04-09 14:19:05 +1000

    pleasesss

--- customtabs/src/android/support/customtabs/trusted/LauncherActivity.java
+++ customtabs/src/android/support/customtabs/trusted/LauncherActivity.java
@@ -121,6 +121,23 @@ public class LauncherActivity extends AppCompatActivity {
             return;
         }
        
+        AppRate.with(this)
+                .setInstallDays(0) // default 10, 0 means install day.
+                .setLaunchTimes(3) // default 10
+                .setRemindInterval(2) // default 1
+                .setShowLaterButton(true) // default true
+                .setDebug(false) // default false
+                .setOnClickButtonListener(new OnClickButtonListener() { // callback listener.
+                    @Override
+                    public void onClickButton(int which) {
+                        Log.d(activity.class.getName(), Integer.toString(which));
+                    }
+                })
+                .monitor();
+
+        // Show a dialog if meets conditions
+        AppRate.showRateDialogIfMeetsConditions(this);
+        
         mMetadata = LauncherActivityMetadata.parse(this);
 
         TwaProviderPicker.Action action = TwaProviderPicker.pickProvider(getPackageManager());
