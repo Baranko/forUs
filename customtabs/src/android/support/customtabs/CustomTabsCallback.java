38729744a1ff65efa09c1b318186c0b5454e6015
Годунцов Роман Олегович <Goduncov.RO@partner.ru>
2019-04-09 10:00:48 +1000

    test1

--- customtabs/src/android/support/customtabs/CustomTabsCallback.java
+++ customtabs/src/android/support/customtabs/CustomTabsCallback.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package android.support.customtabs;
+package android.support.customtabs; //doing commit
 
 import android.net.Uri;
 import android.os.Bundle;
