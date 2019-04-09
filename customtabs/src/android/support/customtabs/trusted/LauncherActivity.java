5182f6dfac8b594944508d3897777ed62471ea23
Baranko <barankotar@gmail.com>
2019-04-10 09:43:33 +1000

    Update LauncherActivity.java

--- customtabs/src/android/support/customtabs/trusted/LauncherActivity.java
+++ customtabs/src/android/support/customtabs/trusted/LauncherActivity.java
@@ -124,10 +124,14 @@ public class LauncherActivity extends AppCompatActivity {
         
         mMetadata = LauncherActivityMetadata.parse(this);
         
+        private boolean testt = false;
+        
         RateThisApp.onCreate(this);
         RateThisApp.showRateDialogIfNeeded(this);
         RateThisApp.Config config = new RateThisApp.Config(3, 5);
         RateThisApp.init(config);
+       
+        
 
 
         TwaProviderPicker.Action action = TwaProviderPicker.pickProvider(getPackageManager());
